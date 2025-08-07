bfcf5bed613c3350b40e5e3d18b68dcf471c1876
l0rem1psum <iswenxuan@gmail.com>
2025-08-07 15:29:11 +0000

    feat: implement skip result for N-1 multiprocessor

--- multiprocessorN-1.go
+++ multiprocessorN-1.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"sync"
@@ -499,9 +500,33 @@ func (fsm *fsmMultiProcessorNIn1OutSync[IO, I, O, _, _, _]) processBatch(inputSl
 func (fsm *fsmMultiProcessorNIn1OutSync[IO, _, O, _, _, _]) handleOutputBatch(outputs []wrappedGenericNIn1OutSyncProcessorOutput[O]) {
 	var io IO
 
+	allSkip := true
+	for _, output := range outputs {
+		if !errors.Is(output.err, SkipResult) {
+			allSkip = false
+		}
+	}
+	if allSkip {
+		return
+	}
+
 	unwrappedOutputs := make([]O, len(outputs))
+	allErrors := true
 	for i, output := range outputs {
 		unwrappedOutputs[i] = output.o
+		if output.err != nil {
+			if errors.Is(output.err, SkipResult) {
+				continue
+			}
+			fsm.subProcessorFSMs[i].logger.With("error", output.err).Error(logProcessingError)
+			fsm.subProcessorFSMs[i].metrics.revertInputProcessedSuccess(context.Background(), 0)
+			fsm.subProcessorFSMs[i].metrics.recordInputProcessedFailure(context.Background(), 0)
+		} else {
+			allErrors = false
+		}
+	}
+	if allErrors {
+		return
 	}
 
 	start := time.Now()
